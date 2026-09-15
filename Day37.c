d3a849d9f4aaf0848a066578fa03501b51aa4141
Bhaskar <bhaskargiri562@gmail.com>
2026-09-15 18:58:21 +0530

    Add files via upload

--- Day37.c
+++ Day37.c
@@ -2,21 +2,19 @@
 
 int main() {
     int r, c, i, j;
-    int a[10][10];
+    int a[10][10], sum[10] = {0};
 
     scanf("%d %d", &r, &c);
 
     for(i = 0; i < r; i++) {
         for(j = 0; j < c; j++) {
             scanf("%d", &a[i][j]);
+            sum[i] += a[i][j];
         }
     }
 
-    for(j = 0; j < c; j++) {
-        for(i = 0; i < r; i++) {
-            printf("%d ", a[i][j]);
-        }
-        printf("\n");
+    for(i = 0; i < r; i++) {
+        printf("%d ", sum[i]);
     }
 
     return 0;
