a2b9400770fb9bcada9b45db3f5f04c9554c1f66
Bhaskar <bhaskargiri562@gmail.com>
2026-09-16 22:05:44 +0530

    Add files via upload

--- Day38.c
+++ Day38.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    int r, c, i, j;
-    int a[10][10], b[10][10], sum[10][10];
+    int n, i, j, symmetric = 1;
+    int a[10][10];
 
-    scanf("%d %d", &r, &c);
+    scanf("%d %d", &n, &j);
 
-    for(i = 0; i < r; i++) {
-        for(j = 0; j < c; j++) {
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
             scanf("%d", &a[i][j]);
         }
     }
 
-    for(i = 0; i < r; i++) {
-        for(j = 0; j < c; j++) {
-            scanf("%d", &b[i][j]);
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                symmetric = 0;
+                break;
+            }
         }
     }
 
-    for(i = 0; i < r; i++) {
-        for(j = 0; j < c; j++) {
-            sum[i][j] = a[i][j] + b[i][j];
-        }
-    }
-
-    for(i = 0; i < r; i++) {
-        for(j = 0; j < c; j++) {
-            printf("%d ", sum[i][j]);
-        }
-        printf("\n");
-    }
+    if(symmetric)
+        printf("True");
+    else
+        printf("False");
 
     return 0;
 }
