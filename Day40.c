a04ca89293f66bd6cbbc118d8fda9b4256cedf6d
Bhaskar <bhaskargiri562@gmail.com>
2026-09-18 19:58:00 +0530

    Add files via upload

--- Day40.c
+++ Day40.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 
 int main() {
-    int n, i, j;
-    int a[10][10];
+    int r1, c1, r2, c2;
+    int i, j, k;
+    int a[10][10], b[10][10], result[10][10] = {0};
 
-    scanf("%d %d", &n, &j);
+    scanf("%d %d", &r1, &c1);
 
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < n; j++) {
+    for(i = 0; i < r1; i++) {
+        for(j = 0; j < c1; j++) {
             scanf("%d", &a[i][j]);
         }
     }
 
-    for(int k = 0; k < 2 * n - 1; k++) {
-        for(i = 0; i < n; i++) {
-            j = k - i;
+    scanf("%d %d", &r2, &c2);
 
-            if(j >= 0 && j < n) {
-                printf("%d ", a[i][j]);
+    for(i = 0; i < r2; i++) {
+        for(j = 0; j < c2; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for(i = 0; i < r1; i++) {
+        for(j = 0; j < c2; j++) {
+            for(k = 0; k < c1; k++) {
+                result[i][j] += a[i][k] * b[k][j];
             }
         }
+    }
+
+    for(i = 0; i < r1; i++) {
+        for(j = 0; j < c2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
     }
 
     return 0;
