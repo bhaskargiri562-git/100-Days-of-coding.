cb27a70dd35e35aef3edad09c43a885580c72419
Bhaskar <bhaskargiri562@gmail.com>
2026-09-21 23:04:05 +0530

    Add files via upload

--- Day43.c
+++ Day43.c
@@ -3,12 +3,23 @@
 
 int main() {
     char str[100];
+    int palindrome = 1;
 
     scanf("%s", str);
 
-    for (int i = strlen(str) - 1; i >= 0; i--) {
-        printf("%c", str[i]);
+    int len = strlen(str);
+
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - i - 1]) {
+            palindrome = 0;
+            break;
+        }
     }
+
+    if (palindrome)
+        printf("Palindrome");
+    else
+        printf("Not palindrome");
 
     return 0;
 }
