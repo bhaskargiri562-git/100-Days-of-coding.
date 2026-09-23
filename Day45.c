eca6bfb08c9a66b3ffe4439d3e4f019a53f9e43e
Bhaskar <bhaskargiri562@gmail.com>
2026-09-23 22:02:08 +0530

    Add files via upload

--- Day45.c
+++ Day45.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    char str[100], ch;
-    int count = 0;
+    char str[100];
 
     scanf("%s", str);
-    scanf(" %c", &ch);
 
     for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == ch) {
-            count++;
-        }
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] = str[i] + 32;
+        else if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
     }
 
-    printf("%d", count);
+    printf("%s", str);
 
     return 0;
 }
